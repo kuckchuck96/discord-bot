185dfc7190b6fb25beb83d980046c0a65cbbb1dd
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2021-04-16 15:20:13 +0530

    Added support for selecting queues

--- cogs/r6stats.py
+++ cogs/r6stats.py
@@ -15,13 +15,13 @@ def get_about_stats(stats):
 def get_general_stats(stats):
     kills = stats['kills']
     deaths = stats['deaths']
-    assists = stats['assists']
+    # assists = stats['assists'] Not available in all queues
     kd = stats['kd']
     wins = stats['wins']
     losses = stats['losses']
     wl = stats['wl']
     matches_played = stats['games_played']
-    return f'**K/D:** {kd}\n**Kills:** {kills}\n**Deaths:** {deaths}\n**Assists:** {assists}\n**W/L:** {wl}\n**Wins:** {wins}\n**Losses:** {losses}\n**Matches Played:** {matches_played}'
+    return f'**K/D:** {kd}\n**Kills:** {kills}\n**Deaths:** {deaths}\n**W/L:** {wl}\n**Wins:** {wins}\n**Losses:** {losses}\n**Matches Played:** {matches_played}'
 
 def get_kills_breakdown_stats(stats):
     headshots = stats['headshots']
@@ -39,10 +39,21 @@ def get_miscellaneous_stats(stats):
     barricades = stats['barricades_deployed']
     return f'**Reinforcements:** {reinforcements}\n**Barricades:** {barricades}\n**Revives:** {revives}\n**Suicides:** {suicides}'
 
-async def embed_stats(ctx, stats):
+def map_queue(queue, stats):
+    if queue == 'casual':
+        return stats['stats']['queue']['casual']
+    elif queue == 'ranked':
+        return stats['stats']['queue']['ranked']
+    elif queue == 'other':
+        return stats['stats']['queue']['other']    
+    # Default 'all'            
+    return stats['stats']['general']
+
+async def embed_stats(ctx, stats, queue):
     try:
         about = get_about_stats(stats)
-        general = get_general_stats(stats['stats']['general'])
+        mapped_queue = map_queue(queue, stats)
+        general = get_general_stats(mapped_queue)
         kills_breakdown = get_kills_breakdown_stats(stats['stats']['general'])
         miscellaneous = get_miscellaneous_stats(stats['stats']['general'])
         # Create embed
@@ -54,7 +65,7 @@ async def embed_stats(ctx, stats):
         embed.set_thumbnail(url= stats['avatar_url_256'])
         embed.set_author(name= stats['username'], icon_url= stats['avatar_url_146'])
         embed.add_field(name= 'About', value= about)
-        embed.add_field(name= 'General', value= general)
+        embed.add_field(name= f'General({queue.capitalize()})', value= general)
         embed.add_field(name= 'Kills Breakdown', value= kills_breakdown)
         embed.add_field(name= 'Miscellaneous', value= miscellaneous)
         await ctx.send(embed=embed)
@@ -71,7 +82,7 @@ class R6Stats(commands.Cog):
         aliases = ['r', 'rainbow', 'rainbow6', 'rainbowsix'],
         help = 'Get R6 stats for LASN'
     )
-    async def get_stats(self, ctx, user, platform = 'pc'):
+    async def get_stats(self, ctx, user, queue = 'all', platform = 'pc'):
         try:
             api_key = os.getenv('R6_STATS_KEY')
             requestUrl = os.getenv('R6_STATS_URL') + f'/stats/{user}/{platform}/generic'
@@ -79,7 +90,7 @@ class R6Stats(commands.Cog):
             statsRes = requests.get(requestUrl, headers = headers)
             if statsRes.status_code != 200:
                 await ctx.send('Please check the arguments passed...')
-            await embed_stats(ctx, statsRes.json())
+            await embed_stats(ctx, statsRes.json(), queue)
         except Exception as err:
             print(err)   
             await ctx.send('Something went wrong, finding someone to blame...probably R6Stats')
