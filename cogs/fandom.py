cad96500a7ca9edd922b13c3b89fc43ab9d72edb
Kunal Deshpande <kunaldeshpande9896@gmail.com>
2021-04-17 21:55:54 +0530

    Introduced marvel character search

--- cogs/fandom.py
+++ cogs/fandom.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 import hashlib
 import os
 import requests
+from types import SimpleNamespace
+import json
+from datetime import datetime as dt
 
 
 class Fandom(commands.Cog):
@@ -13,15 +16,17 @@ class Fandom(commands.Cog):
         self.marv_env_list = ['MARV_TS', 'MARV_PRIVATE_KEY', 'MARV_PUBLIC_KEY']
 
     @commands.command(
-        name='marvel'
+        name='marvel',
+        help='Search for your favourite Marvel character.'
     )
-    async def get_all_marvel_characters(self, ctx, arg) -> None:
-        # await ctx.send(self.create_hash())
+    async def get_marvel_character(self, ctx, *arg) -> None: 
+        char_name = '-'.join(list(map(lambda c: c.capitalize(), arg[0].split('-')))) if len(arg) == 1 and str(arg[0]).count('-') else ' '.join(list(map(lambda c: c.capitalize(), arg)))  
+
         body = [
             ('ts', os.getenv('MARV_TS')),
             ('apikey', os.getenv('MARV_PUBLIC_KEY')),
             ('hash', self.create_hash()),
-            ('name', arg)
+            ('name', char_name)
         ]
 
         try:
@@ -32,18 +37,28 @@ class Fandom(commands.Cog):
             print(ex)
             await ctx.send(ex)
         else:   
-            results = res.json()['data']['results'][0]
-            card = discord.Embed(
-                title=results['name'],
-                description=results['description'],
-                color = discord.Color.red()
-            )
-            card.set_footer(text=res.json()['attributionText'])
-            for f in ['comics', 'series', 'stories', 'events']:
-                card.add_field(name=f.capitalize(), value=results[f]['available'])
-            await ctx.send(embed=card)
-            
+            # Convert json response to object.
+            obj = json.loads(res.text, object_hook=lambda o: SimpleNamespace(**o))
 
+            if len(obj.data.results) > 0:
+                # Fetch most used attribute.
+                results = obj.data.results[0]
+
+                card = discord.Embed(
+                    title=results.name,
+                    description=results.description,
+                    color = discord.Color.red(),
+                    timestamp=dt.strptime(results.modified.split('T')[0], '%Y-%m-%d')
+                )
+                card.set_footer(text=f'* {obj.attributionText}')
+                thumbnail = results.thumbnail
+                card.set_thumbnail(url=f'{thumbnail.path}/portrait_medium.{thumbnail.extension}')
+                for f in ['comics', 'series', 'stories', 'events']:
+                    card.add_field(name=f.capitalize(), value=eval(f'results.{f}.available'))
+                await ctx.send(embed=card)
+            else:
+                await ctx.send(f'Cannot fetch dets for **{char_name}**.')
+            
     def create_hash (self):
         hash = hashlib.md5()
         for k in self.marv_env_list:
