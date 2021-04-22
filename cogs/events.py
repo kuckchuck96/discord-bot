57b07ee7483fc9e8901cc865119503750665aedf
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2021-04-23 02:14:44 +0530

    Added useful listeners

--- cogs/events.py
+++ cogs/events.py
@@ -7,6 +7,13 @@ class Events(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
+    async def on_connect(self):
+        try:
+            print('Connecting to discord...')   
+        except Exception as ex:
+            print(f'Discord connection error: {ex}')
+        
+    @commands.Cog.listener()
     async def on_ready(self):
         await self.bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name='lasangiri'))
 
@@ -17,7 +24,11 @@ class Events(commands.Cog):
             
             await self.bot.user.edit(avatar=image)
 
-        print(self.bot.user.name + ' is online.')
+        print(f'{self.bot.user.name} is online | Servers: {len(self.bot.guilds)}')
+
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        print(f"[Command] {ctx.message.clean_content}")
 
 def setup(bot):
     bot.add_cog(Events(bot))        
