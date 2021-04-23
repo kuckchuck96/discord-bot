f58f63ddb571d150322ed82b55231d50e5947e73
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2021-04-23 18:39:33 +0530

    Event listener updates (#18)
    * Added useful listeners
    
    * Message delete event listener
    
    * Disconnect and reconnect events added

--- cogs/events.py
+++ cogs/events.py
@@ -7,6 +7,21 @@ class Events(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
+    async def on_connect(self):
+        try:
+            print('Connecting to discord...')   
+        except Exception as ex:
+            print(f'Discord connection error: {ex}')
+
+    @commands.Cog.listener()
+    async def on_disconnect(self):
+        print(f'Disconnected from discord...reconnecting...') 
+
+    @commands.Cog.listener()
+    async def on_resumed(self):
+        print(f'Reconnected to discord.')           
+        
+    @commands.Cog.listener()
     async def on_ready(self):
         await self.bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name='lasangiri'))
 
@@ -17,7 +32,16 @@ class Events(commands.Cog):
             
             await self.bot.user.edit(avatar=image)
 
-        print(self.bot.user.name + ' is online.')
+        print(f'{self.bot.user.name} is online | Servers: {len(self.bot.guilds)}')
+
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        print(f"[Command] {ctx.message.clean_content}")
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        await message.channel.send(f'I see everything {message.author.mention}')  
+        await message.channel.send('http://gph.is/12wV6iC') # Eye of Sauron
 
 def setup(bot):
     bot.add_cog(Events(bot))        
