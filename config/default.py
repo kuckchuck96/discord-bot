f7d976fa3ede086ba67e704f578c88f61d0d389d
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2021-04-25 14:58:48 +0530

    Isolated common config from env dependent

--- config/default.py
+++ config/default.py
@@ -7,11 +7,15 @@ def config():
     try:
         __location__ = os.path.realpath(
             os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        base_filename = 'base_config'
         filename = 'dev_config'
         if os.getenv('DEPLOY_ENV') == 'prod':
             filename = 'prod_config'
-        
-        json_file = open(os.path.join(__location__ , f"{filename}.json"), encoding='utf8')
-        return json.loads(json_file.read(), object_hook=lambda o: SimpleNamespace(**o))
+        base_config = open(os.path.join(__location__ , f'{base_filename}.json'), encoding='utf8')
+        base_config = json.loads(base_config.read(), object_hook=lambda o: SimpleNamespace(**o))
+        env_config = open(os.path.join(__location__ , f'{filename}.json'), encoding='utf8')
+        env_config = json.loads(env_config.read(), object_hook=lambda o: SimpleNamespace(**o))
+        # Chain env dependent config with base config
+        return SimpleNamespace(**base_config.__dict__, **env_config.__dict__)
     except FileNotFoundError:
         raise FileNotFoundError("JSON file wasn't found")
