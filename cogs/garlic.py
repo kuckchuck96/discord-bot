659a9546177d0d0ef5e24bfb0625c0834dea10c5
Kunal Deshpande <50999920+kuckchuck96@users.noreply.github.com>
2021-04-26 11:07:36 +0530

    ADDED: Tenor API, bye GIPHY. (#25)
    * Tenor API
    
    Added new GIF API (Tenor).
    
    * Updated base_config.json
    
    Added Tenor API.
    
    * Refactoring
    
    1. Removed unused imports.
    2. Removed GIPHY URL and key.

--- cogs/garlic.py
+++ cogs/garlic.py
@@ -1,8 +1,9 @@
-import os
+import json
 import requests
 import random
 import re
-import config
+import discord
+from types import SimpleNamespace
 
 from discord.ext import commands
 from config import default
@@ -12,8 +13,8 @@ class Garlic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         config = default.config()
-        self.gify_api_key = config.gify.api_key
-        self.gify_api_url = config.gify.api_url
+        self.tenor_api_key = config.tenor.api_key
+        self.tenor_api_url = config.tenor.api_url
 
     @commands.command(
         name='greet',
@@ -27,21 +28,33 @@ class Garlic(commands.Cog):
         aliases = ['g'],
         help='Send GIFs based on the search text passed.'
         )
-    async def send_gif(self, ctx, arg):
-        body = [
-            ('api_key', self.gify_api_key),
-            ('q', arg),
+    async def send_gif(self, ctx, *arg):
+        params = [
+            ('key', self.tenor_api_key),
+            ('q', ' '.join(arg)),
             ('limit', 50),
-            ('lang', 'en')
+            ('anon_id', ctx.author.id)
         ]
-        res = requests.get(self.gify_api_url, params=body)
-        if res.status_code != 200:
-            await ctx.send('Oops! Something went wrong. Please try again.')
-        elif len(res.json()['data']) > 0:
-            gif = res.json()['data'][random.randrange(0, len(res.json()['data']))]
-            await ctx.send(gif['embed_url'])
+
+        try:
+            res = requests.get(self.tenor_api_url, params=params)
+            if res.status_code != 200:
+                raise Exception('Unable to get GIF for ' + ' '.join(arg))
+        except Exception as ex:
+            print(ex)
+            await ctx.send(ex)
         else:
-            await ctx.send('No results found for keyword "{0}".'.format(arg))
+            obj = json.loads(res.text, object_hook=lambda o: SimpleNamespace(**o))
+            results = obj.results
+            result = results[random.randrange(0, len(results))]
+            if result != None:
+                embed = discord.Embed()
+                embed.set_image(url=result.media[0].gif.url)
+                embed.set_footer(text='via Tenor', icon_url='https://www.gstatic.com/tenor/web/attribution/PB_tenor_logo_blue_vertical.png')
+                print(f'{ctx.author.name} requested a gif.')
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send('No results exist for ' + ' '.join(arg))
 
     @commands.command(
         name='insult',
