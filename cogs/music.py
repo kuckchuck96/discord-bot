ecb6e25651958f67ed9caf941ce546603a2cbf6b
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2021-05-07 18:59:06 +0530

    Music player updates

--- cogs/music.py
+++ cogs/music.py
@@ -1,5 +1,4 @@
 import asyncio
-
 import discord
 import youtube_dl
 
@@ -7,7 +6,6 @@ from discord.ext import commands
 
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
-
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
@@ -21,18 +19,7 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0', # bind to ipv4 since ipv6 addresses cause issues sometimes,
-   
-
 }
-
-# ytdl_format_options = {
-#     'format': 'bestaudio/best',
-#     'postprocessors': [{
-#         'key': 'FFmpegExtractAudio',
-#         'preferredcodec': 'mp3',
-#         'preferredquality': '192',
-#     }],
-# }
 
 ffmpeg_options = {
     'options': '-vn'
@@ -40,21 +27,19 @@ ffmpeg_options = {
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
-
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
         super().__init__(source, volume)
 
         self.data = data
-
         self.title = data.get('title')
         self.url = data.get('url')
+        self.thumbnail = data.get('thumbnail')
 
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
         if 'entries' in data:
             # take first item from a playlist
             data = data['entries'][0]
@@ -66,30 +51,35 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def embed_stream(self, ctx, player):
+        # Embeds have 1024 char limit
+        playing_title = f'[{player.title}]({player.url})'[0:1024] 
+        embed = discord.Embed(
+            name = 'LASNBot',
+            title = 'Now Playing',
+            color = discord.Color.dark_red()
+        )
+        embed.set_thumbnail(url= player.thumbnail)
+        embed.add_field(name= '\u200b', value= playing_title)  
+        await ctx.send(embed=embed)     
+
     @commands.command(name = 'join',
         aliases = ['aaja'],
         help = 'Ask LASN to grace us with his company')
     async def join(self, ctx, *, channel: discord.VoiceChannel):
         #To Join Voice Channel
-
         if ctx.voice_client is not None:
             return await ctx.voice_client.move_to(channel)
-
         await channel.connect()
 
     @commands.command(name = 'stream',
         aliases = ['baja'],
         help = 'Stream music from youtube')
     async def stream(self, ctx, *, url):
-       #Stream
-
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
             ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
-
-        await ctx.send(f'Now playing: {player.title}')
-
-        
+        await self.embed_stream(ctx, player)
 
     @commands.command(name = 'pause',
         aliases = ['pse'],
@@ -107,35 +97,20 @@ class Music(commands.Cog):
         if ctx.voice_client is None:
             return await ctx.send("Not connected to a voice channel.")
         ctx.voice_client.resume()
-        
-
 
     @commands.command(name = 'volume',
         aliases = ['vol'],
         help = 'Change lasn ')
     async def volume(self, ctx, volume: int):
-       #Change Vol
-
+        #Change Vol
         if ctx.voice_client is None:
             return await ctx.send("Not connected to a voice channel.")
-
         ctx.voice_client.source.volume = volume / 100
         await ctx.send(f"Changed volume to {volume}%")
-
-    
-
-
-
-  
-
-
-
-
 
     @commands.command(help= 'Disconnect from voice channel')
     async def stop(self, ctx):
         #Remove Bot
-
         await ctx.voice_client.disconnect()
 
     # @play.before_invoke
@@ -150,7 +125,6 @@ class Music(commands.Cog):
                 raise commands.CommandError("Author not connected to a voice channel.")
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
-
 
 def setup(bot):
     bot.add_cog(Music(bot)) 
