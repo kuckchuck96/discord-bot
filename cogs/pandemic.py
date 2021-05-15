e2d9c76eb402511afb0ff332230b77d639143d26
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2021-05-15 18:12:09 +0530

    Vaccine updates (#32)
    * Scrapes successive dates for vaccine availablity
    
    * Scrape count configurable
    
    * Removed multiple calls and showing results for 7 days
    
    * Removed unused config
    
    * Unused config references removed
    
    * Code deduplication

--- cogs/pandemic.py
+++ cogs/pandemic.py
@@ -1,13 +1,13 @@
-from datetime import datetime
+import datetime
 import json
-from types import SimpleNamespace
 import discord
-from requests.api import head
 import config
 import requests
 
+from types import SimpleNamespace
 from discord.ext import commands
 from config import default
+
 
 class Pandemic(commands.Cog):
     def __init__(self, bot):
@@ -69,58 +69,95 @@ class Pandemic(commands.Cog):
             return await self.embed_content(ctx, data)
         except Exception as err:
             print(err)
-            await ctx.send('Something went wrong, finding someone to blame...')      
+            await ctx.send('Something went wrong, finding someone to blame...')
+
+    async def request_vaccine_availability(self, ctx, params):
+        try:
+            res = requests.get(url=self.cowin_api_url, params=params)
+            if not res.ok:
+                raise Exception('Unable to get vaccination centers.')
+            return json.loads(res.text, object_hook=lambda o: SimpleNamespace(**o))
+        except Exception as ex:
+            await ctx.send(ex)
+
+    async def add_embed_fields(self, center, session, date):
+        embed = discord.Embed(
+            title=f'{ center.name }, { center.district_name }, { center.state_name }',
+            colour=discord.Color.magenta(),
+            description='**📍Address:** {0}\n**🏢 Block name:** {1}\n🗓️ **{2}**\n'.format(center.address, center.block_name, date if len(center.block_name) else 'NA'),
+        )
+        embed.add_field(name='💰 Fees', value=center.fee_type)
+        embed.add_field(name='🏟️ Capacity', value=session.available_capacity)
+        embed.add_field(name='{0} Age'.format('👪' if session.min_age_limit <= 18 else '👨👩'), value=f'{session.min_age_limit}+')
+        embed.add_field(name='💉 Vaccine', value=session.vaccine)
+        embed.add_field(name='⏱️ Slots', value='\n'.join(f'{i+1}) {s}' for i, s in enumerate(session.slots)))
+        embed.set_footer(text='Powered by Co-WIN Public APIs, A GoI Initiative.', icon_url='https://www.countryflags.io/in/flat/64.png')
+        return embed
+
+    async def construct_embeds(self, centers, date, week):
+        incoming_date = date.split('-')
+        embeds = list()
+        # Iterating to match the date passed as a parameter.
+        if week:
+            for center in centers:
+                for session in center.sessions:
+                    embed = await self.add_embed_fields(center, session, date)
+                    embeds.append(embed)
+        else:
+            for center in centers:
+                for session in center.sessions:
+                    if all(int(x) == int(y) for x, y in zip(incoming_date, str(session.date).split('-'))):
+                        embed = await self.add_embed_fields(center, session, date)
+                        embeds.append(embed)
+        return embeds            
+
+
+    async def render_embeds_by_date(self, ctx, centers, date, pincode):
+        embeds = await self.construct_embeds(centers, date, False)
+        if len(embeds):
+            for e in embeds:
+                await ctx.send(embed=e)
+        else:
+            await ctx.send(f'Sorry! No vaccination centers available near **{pincode}** on **{date}**')
+
+    async def render_embeds_for_week(self, ctx, centers, pincode, date_now, seventh_day):
+        embeds = await self.construct_embeds(centers, date_now, True)
+        if len(embeds):
+            for e in embeds:
+                await ctx.send(embed=e)
+        else:
+            await ctx.send(f'Sorry! No vaccination centers available near **{pincode}** from **{date_now} - {seventh_day}**')
+
+    async def get_7th_successive_date(self):
+        date_now = datetime.date.today()
+        seventh_day = date_now + datetime.timedelta(days=7)
+        return { 'now': date_now.strftime("%d-%m-%Y"), 'seventh_day': seventh_day.strftime("%d-%m-%Y") }
 
     @commands.command(
         name='vaccine',
         help='Provides Covid-19 vaccination centers based on \'pincode\' and \'date\' (dd-mm-yyyy).'
     )
-    async def vaccine_availability(self, ctx, *arg):
-        pincode, date = map(str, arg)
-
+    async def vaccine_availability(self, ctx, pincode, date = ''):
         params = {
             'pincode': pincode,
             'date': date
         }
-
-        try:
-            res = requests.get(url=self.cowin_api_url, params=params)
-            
-            if not res.ok:
-                raise Exception('Unable to get vaccination centers.')
-        except Exception as ex:
-            await ctx.send(ex)
-        else:
-            centers = json.loads(res.text, object_hook=lambda o: SimpleNamespace(**o))
-
+        if not len(date):
+            interval = await self.get_7th_successive_date()
+            date_now = interval['now']
+            seventh_day = interval['seventh_day']
+            params['date'] = date_now
+            centers = await self.request_vaccine_availability(ctx, params)
+            await ctx.send(f'Searching for sessions between **{date_now} - {seventh_day}**...')
             if len(centers):
-
-                incoming_date = date.split('-')
-                embeds = list()
-
-                # Iterating to match the date passed as a parameter.
-                for center in centers:
-                    for session in center.sessions:
-                        if all(int(x) == int(y) for x, y in zip(incoming_date, str(session.date).split('-'))):
-                            embed = discord.Embed(
-                                title=f'{ center.name }, { center.district_name }, { center.state_name }',
-                                colour=discord.Color.magenta(),
-                                description='**📍Address:** {0}\n**🏢 Block name:** {1}\n'.format(center.address, center.block_name if len(center.block_name) else 'NA'),
-                            )
-                            embed.add_field(name='💰 Fees', value=center.fee_type)
-                            embed.add_field(name='🏟️ Capacity', value=session.available_capacity)
-                            embed.add_field(name='{0} Age'.format('👪' if session.min_age_limit <= 18 else '👨👩'), value=f'{session.min_age_limit}+')
-                            embed.add_field(name='💉 Vaccine', value=session.vaccine)
-                            embed.add_field(name='⏱️ Slots', value='\n'.join(f'{i+1}) {s}' for i, s in enumerate(session.slots)))
-                            embed.set_footer(text='Powered by Co-WIN Public APIs, A GoI Initiative.', icon_url='https://www.countryflags.io/in/flat/64.png')
-                            embeds.append(embed)
-                            break
-
-                if len(embeds):
-                    for e in embeds:
-                        await ctx.send(embed=e)
-                else:
-                    await ctx.send(f'Sorry! No vaccination centers available near **{pincode}** on **{date}**')
+                await self.render_embeds_for_week(ctx, centers, pincode, date_now, seventh_day)
+            else:
+                await ctx.send(f'Sorry! No vaccination centers available near **{pincode}** from **{date_now} - {seventh_day}**')
+        else:    
+            centers = await self.request_vaccine_availability(ctx, params)
+            await ctx.send(f'Searching for sessions on **{date}**...')
+            if len(centers):
+                await self.render_embeds_by_date(ctx, centers, date, pincode)
             else:
                 await ctx.send(f'Sorry! No vaccination centers available near **{pincode}** on **{date}**')
 
