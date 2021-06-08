d4659901487ac6379ab6d1a120086ff0099e8b1c
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2021-06-08 23:02:37 +0530

    Game activity notifications refactor (#36)
    * generalised game notifications
    
    * game activity notification refactor

--- utils/notify.py
+++ utils/notify.py
@@ -1,24 +1,39 @@
 import discord
 import config
 
+from discord.enums import ActivityType
 from config import default
 
 config = default.config()
 
-async def r6notify(bot, member):
-    ''' Notify when a user starts playing R6 '''
-    if member.activity != None: #Ignore other detail events
-        if (member.activity.name == "Tom Clancy's Rainbow Six Siege" or member.activity.name == "Rainbow Six Siege") and member.activity.details == None:
-            channel = bot.get_channel(config.r6_notify_channel)
-            embed = embed_r6_notification(member)
-            await channel.send(embed=embed)
+async def send_notification_to_channel(bot, member):
+    ''' Get channel and send embed ''' 
+    channel = bot.get_channel(config.r6_notify_channel)
+    embed = embed_notification(member)
+    await channel.send(embed=embed)
 
-def embed_r6_notification(member):
-    ''' Create embed for r6 notification '''
+def embed_notification(member):
+    ''' Create embed for game notification '''
     embed = discord.Embed(
         name = 'LASNBot',
-        title = f'{member.name} started playing Tom Clancy\'s Rainbow Six Siege',
-        description = f'Join now!',
+        title = f'{member.name} started playing {member.activity.name}',
         color = discord.Color.red()
     )
     return embed   
+
+async def notify_game_activity(bot, before, after):
+    ''' Notify game activity to configured channel '''
+    if before.activity and before.activity.type != ActivityType.playing:
+        return None
+
+    if after.activity and after.activity.type != ActivityType.playing:
+        return None    
+
+    if not before.activity and after.activity and after.activity.type == ActivityType.playing:
+        # Add start playing logic like DB update for roulette
+        print(f'[Event] {before} > playing > {after.activity.name}')  
+        await send_notification_to_channel(bot, after)
+
+    if before.activity and before.activity.type == ActivityType.playing and not after.activity:
+        # Add stop playing logic like DB update for roulette
+        print(f'[Event] {before} > stopped playing')                  
